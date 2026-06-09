d81d15865be1fc25d7574e3021818bdbc92905df
SolarRadiannt <naufal.jf2108@gmail.com>
2026-06-09 20:03:01 +0700

    assocngeneric

--- src/exercises/assocngeneric.rs
+++ src/exercises/assocngeneric.rs
@@ -13,25 +13,47 @@
 // You don't have to though: it's perfectly okay to write three separate
 // implementations manually. Venture further only if you're curious.
 
+use num_traits::Pow;
+
+trait Power<E> {
+	fn power(&self, n: E) -> u32;
+}
+
+impl Power<u16> for u32 {
+	fn power(&self, n: u16) -> u32 {
+		self.pow(n)
+	}
+}
+impl Power<u32> for u32 {
+	fn power(&self, n: u32) -> u32 {
+		self.pow(n)
+	}
+}
+impl Power<&u32> for u32 {
+	fn power(&self, n: &u32) -> u32 {
+    	self.pow(n)
+	}
+}
+
 #[cfg(test)]
 mod tests {
     use super::Power;
-
+    
     #[test]
     fn test_power_u16() {
-        let x: u32 = 2_u32.power(3u16);
+        let x: u32 = 2_u32.power(3_u16);
         assert_eq!(x, 8);
     }
 
     #[test]
     fn test_power_u32() {
-        let x: u32 = 2_u32.power(3u32);
+        let x: u32 = 2_u32.power(3_u32);
         assert_eq!(x, 8);
     }
 
     #[test]
     fn test_power_ref_u32() {
-        let x: u32 = 2_u32.power(&3u32);
+        let x: u32 = 2_u32.power(&3_u32);
         assert_eq!(x, 8);
     }
 }
